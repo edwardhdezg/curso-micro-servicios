dd46103c0daf79e2ba6aa75c413b1f802e089c2c
Eduardo Hernandez <63613312+edwardhdezg@users.noreply.github.com>
2024-06-27 10:51:51 -0600

    Actualizacion del codigo- trabajando con docker
    Actualizacion del codigo - traajando con docker

--- msvc-cursos/src/main/java/org/course/springcloud/mvsc/cursos/clients/UsuarioClientRest.java
+++ msvc-cursos/src/main/java/org/course/springcloud/mvsc/cursos/clients/UsuarioClientRest.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-usuarios", url = "localhost:8001")
+@FeignClient(name = "msvc-usuarios", url = "${msvc.usuarios.url}")
 public interface UsuarioClientRest {
     @GetMapping("/{id}")
     Usuario detalle(@PathVariable Long id);
